51a4249f510136924065d3877b6848223c88868d
augustin <augustin.gardette@gmail.com>
2014-12-22 23:41:51 +0100

    ui waste drop hotel

--- SeriousGame/Assets/Scripts/WasteDropHotel/WasteRespawnScript.cs
+++ SeriousGame/Assets/Scripts/WasteDropHotel/WasteRespawnScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WasteRespawnScript : MonoBehaviour {
 
@@ -7,18 +8,25 @@ public class WasteRespawnScript : MonoBehaviour {
 	public GameObject paperDumpster;
 	public GameObject plasticDumpster;
 	public GameObject glassDumpster;
-	public GameObject[] paperWaste = new GameObject[2];
+/*	public GameObject[] paperWaste = new GameObject[2];
 	public GameObject[] plasticWaste = new GameObject[2];
-	public GameObject[] glassWaste = new GameObject[2];
-
+	public GameObject[] glassWaste = new GameObject[2];*/
+	public List<GameObject> wasteList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		
+		PlayerPrefs.SetInt("Score", 12358);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
+	}
+
+	public void DropWaste()
+	{
+		var randomWasteIndex = Random.Range(0, this.wasteList.Count);
+		
 	}
 }
