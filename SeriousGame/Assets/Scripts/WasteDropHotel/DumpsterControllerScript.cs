e70cd500051e16bad5cede5c83a34b066ff99c52
augustin <augustin.gardette@gmail.com>
2015-01-10 19:23:53 +0100

    - Remove dumpster carrousel selection - Add new dumpster selection method - Add UI bonus on dumpster
    Signed-off-by: augustin <augustin.gardette@gmail.com>

--- SeriousGame/Assets/Scripts/WasteDropHotel/DumpsterControllerScript.cs
+++ SeriousGame/Assets/Scripts/WasteDropHotel/DumpsterControllerScript.cs
@@ -1,18 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//public enum WasteType { Plastic, Paper, Glass};
+
 public class DumpsterControllerScript : MonoBehaviour {
 
 	public KeyCode moveLeftKeyCode = KeyCode.LeftArrow;
 	public KeyCode moveRightKeyCode = KeyCode.RightArrow;
+	public KeyCode paperDumpsterKeyCode = KeyCode.W;
+	public KeyCode plasticDumpsterKeyCode = KeyCode.X;
+	public KeyCode glassDumpsterKeyCode = KeyCode.C;
+
 
 	public  bool _wantToMoveLeft = false;
 	public bool _wantToMoveRight = false;
+	public GameObject _paperDumpster;
+	public GameObject _plasticDumpster;
+	public GameObject _glassDumpster;
 
+	private float _moveAmount;
+	private GameStateWasteDropHotel GS;
+	private WasteType targetWaste = WasteType.Paper;
+
+
+	void Awake()
+	{
+		GS = GameStateWasteDropHotel.Instance;
+		_moveAmount = GS.HotelScale;
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		Transform[] tranDumps = this.GetComponentsInChildren<Transform>();
+		foreach(Transform t in tranDumps)
+		{
+			//Debug.Log("*"+t.tag+"*");
+			if(t.tag == "Paper")
+			{
+				_paperDumpster = t.gameObject;
+			}
+			else
+			{
+				if(t.tag == "Plastic")
+				{
+					_plasticDumpster = t.gameObject;
+				}
+				else
+				{
+					if( t.tag == "Glass")
+					{
+						_glassDumpster = t.gameObject;
+					}
+				}
+			}
+		}
+		SetActiveDumpster(targetWaste);
 	}
 	
 	// Update is called once per frame
@@ -40,6 +82,21 @@ public class DumpsterControllerScript : MonoBehaviour {
 			_wantToMoveRight = false;
 		}
 
+		if(Input.GetKeyDown(paperDumpsterKeyCode))
+		{
+			SetActiveDumpster(WasteType.Paper);
+		}
+
+		if(Input.GetKeyDown(plasticDumpsterKeyCode))
+		{
+			SetActiveDumpster(WasteType.Plastic);
+		}
+
+		if(Input.GetKeyDown(glassDumpsterKeyCode))
+		{
+			SetActiveDumpster(WasteType.Glass);
+		}
+
 	}
 
 	void FixedUpdate()
@@ -47,14 +104,14 @@ public class DumpsterControllerScript : MonoBehaviour {
 		if(_wantToMoveLeft)
 		{
 			var prevPos = this.transform.position;
-			this.transform.position = prevPos + Vector3.left;
+			this.transform.position = prevPos + Vector3.left * _moveAmount;
 			_wantToMoveLeft = false;
 		}
 
 		if(_wantToMoveRight)
 		{
 			var prevPos = this.transform.position;
-			this.transform.position = prevPos + Vector3.right;
+			this.transform.position = prevPos + Vector3.right * _moveAmount;
 			_wantToMoveRight = false;
 		}
 	}
@@ -62,5 +119,20 @@ public class DumpsterControllerScript : MonoBehaviour {
 	void OnCollisionEnter(Collision col)
 	{
 		Debug.Log(col.gameObject.tag);
+	}
+
+	public void SetActiveDumpster(WasteType type)
+	{
+		targetWaste = type;
+		_paperDumpster.SetActive((type == WasteType.Paper));
+		_plasticDumpster.SetActive((type == WasteType.Plastic));
+		_glassDumpster.SetActive((type == WasteType.Glass));
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log("Collid");
+		Debug.Log(col.tag);
+		Destroy(col.gameObject);
 	}
 }
---
+++ SeriousGame/Assets/Scripts/WasteDropHotel/GameStateWasteDropHotel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateWasteDropHotel{
+
+	protected GameStateWasteDropHotel(){}
+
+	private static GameStateWasteDropHotel _instance = null;
+
+	public static GameStateWasteDropHotel Instance {
+		get
+		{
+			if(_instance == null)
+			{
+				_instance = new GameStateWasteDropHotel();
+			}
+			return _instance;
+		}
+
+	}
+
+	private float _hotelScale = 2.0f;
+
+	public float HotelScale {
+		get {
+			return _hotelScale;
+		}
+		set {
+			_hotelScale = value;
+		}
+	}
+
+}
